7f2c3c120aac02be807c1cd4c10e6b0388ea3459
IBRAHIM-LOKMAN\ibrah <ibrahimlokman.bd@gmail.com>
2024-01-19 22:54:19 +0600

    Using Intent from fragment.

--- app/src/main/java/com/ibrahim/recordkeeperapp/RunningFragment.kt
+++ app/src/main/java/com/ibrahim/recordkeeperapp/RunningFragment.kt
@@ -1,5 +1,6 @@
 package com.ibrahim.recordkeeperapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -18,4 +19,25 @@ class RunningFragment : Fragment(){
         binding = FragmentRunningBinding.inflate(inflater, container, false)
          return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.container5km.setOnClickListener {
+          lanunchRunningRecordActivity()
+        }
+    }
+
+    private fun lanunchRunningRecordActivity() {
+
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
+
 }
